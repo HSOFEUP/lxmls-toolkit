5ab529afa8d0eac1736273d242795f610149d354
Ramón Astudillo <ramon@astudillo.com>
2013-07-18 17:06:43 +0100

    Small improvements to Day 3 test script
    Exercise number is now printed out as in Day 2, added paths needed to run scripts from ./lxmls/

--- lxmls/labs/day3.py
+++ lxmls/labs/day3.py
@@ -1,3 +1,12 @@
+import sys
+sys.path.append('.')
+sys.path.append('..')
+import ipdb
+
+print "------------"
+print "Exercise 3.1"
+print "------------"
+
 import sequences.crf_online as crfo
 import sequences.structured_perceptron as spc
 import readers.pos_corpus as pcc
@@ -10,6 +19,14 @@ corpus = pcc.PostagCorpus()
 train_seq = corpus.read_sequence_list_conll("../data/train-02-21.conll",max_sent_len=10, max_nr_sent=1000)
 test_seq = corpus.read_sequence_list_conll("../data/test-23.conll",max_sent_len=10, max_nr_sent=1000)
 dev_seq = corpus.read_sequence_list_conll("../data/dev-22.conll",max_sent_len=10, max_nr_sent=1000)
+
+# Structure of the class containing sequences
+# train_seq.x_dict
+# train_seq.y_dict
+# train_seq.seq_list
+# train_seq.seq_list[0].sequence_list
+# train_seq.seq_list[0].x
+# train_seq.seq_list[0].y
 
 feature_mapper = idfc.IDFeatures(train_seq)
 feature_mapper.build_features()
@@ -27,6 +44,14 @@ eval_test = crf_online.evaluate_corpus(test_seq, pred_test)
 
 print "CRF - ID Features Accuracy Train: %.3f Dev: %.3f Test: %.3f"%(eval_train, eval_dev, eval_test)
 
+# Error analysis
+import sequences.confusion_matrix as cm
+confusion_matrix = cm.build_confusion_matrix(test_seq.seq_list,pred_test,len(corpus.tag_dict), crf_online.get_num_states())
+cm.plot_confusion_bar_graph(confusion_matrix, corpus.tag_dict,xrange(crf_online.get_num_states()), 'Confusion matrix')
+
+print "------------"
+print "Exercise 3.2"
+print "------------"
 
 feature_mapper = exfc.ExtendedFeatures(train_seq)
 feature_mapper.build_features()
@@ -44,7 +69,15 @@ eval_test = crf_online.evaluate_corpus(test_seq, pred_test)
 
 print "CRF - Extended Features Accuracy Train: %.3f Dev: %.3f Test: %.3f"%(eval_train, eval_dev,eval_test)
 
+# Error analysis
+import sequences.confusion_matrix as cm
+confusion_matrix = cm.build_confusion_matrix(test_seq.seq_list,pred_test,len(corpus.tag_dict), crf_online.get_num_states())
+cm.plot_confusion_bar_graph(confusion_matrix, corpus.tag_dict,xrange(crf_online.get_num_states()), 'Confusion matrix')
 
+
+print "------------"
+print "Exercise 3.3"
+print "------------"
 
 print "Perceptron Exercise"
 
